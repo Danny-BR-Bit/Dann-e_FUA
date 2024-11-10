b06a80667b3ee0847d82e011a079395423ca5439
Pancito <jontk2006@gmail.com>
2024-11-10 17:22:23 -0500

    Actualizacion primer codigo del programa

--- functions.py
+++ functions.py
@@ -1,1 +1,10 @@
 #Aqui van las funciones que usemos para esto :D
+def promedio_notas(cantidad_notas):
+    cont = 1
+    prom = []
+    while cont <= cantidad_notas:
+        nota = float(input(f"Ingrese su nota {cont}: "))
+        prom.append(nota)
+        cont += 1
+    promedio = round(sum(prom)/len(prom),1)
+    return promedio
--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 #Idea principal: una calculadora de notas que permita calcular el promedio de n cantidad de notas.
 #Ideas secundarias: que se pueda elegir entre un promedio de notas que solo reciba notas cuantitativas y otro que solo reciba notas cualitativas; 
+
+from functions import promedio_notas
+
+cantidad_notas = int(input("Ingrese la cantidad de notas que va a promediar: "))
+
+print(promedio_notas(cantidad_notas))
+
