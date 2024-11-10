f9ec4729ac2cd95a90c7c40ee70b917ec8c842e6
Pancito <jontk2006@gmail.com>
2024-11-10 18:27:02 -0500

    Adicion sistema de decision (si es cualitativo o cuantitativo)

--- functions.py
+++ functions.py
@@ -1,10 +1,15 @@
 #Aqui van las funciones que usemos para esto :D
-def promedio_notas(cantidad_notas):
+def promedio_notas_cuantitativas(cantidad_notas):
     cont = 1
     prom = []
     while cont <= cantidad_notas:
+        print("")
         nota = float(input(f"Ingrese su nota {cont}: "))
-        prom.append(nota)
-        cont += 1
+        if 0 <= nota <= 5:  #Aqui confirma que la nota este entre 0 y 5, y si no lo esta dice que no esta y no cuenta el valor ingresado dentro de las notas
+            prom.append(nota)
+            cont += 1
+        else:
+            print("La nota debe estar entre 0.0 y 5.0")
+        
     promedio = round(sum(prom)/len(prom),1)
     return promedio
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 #Idea principal: una calculadora de notas que permita calcular el promedio de n cantidad de notas.
 #Ideas secundarias: que se pueda elegir entre un promedio de notas que solo reciba notas cuantitativas y otro que solo reciba notas cualitativas; 
+try:
 
-from functions import promedio_notas
+    from functions import promedio_notas_cuantitativas
 
-cantidad_notas = int(input("Ingrese la cantidad de notas que va a promediar: "))
-
-print(promedio_notas(cantidad_notas))
+    print("\n"+">"*17+" ¡Bienvenido a Dann-E, tu calculadora de notas de confianza! "+"<"*17+"\n")
+    print("¿Que proceso deseas realizar hoy?"+"\n"+"\n"+"1. Calculo de promedio de notas cuantitativas."+"\n"+"2. Calculo de promedio de notas cualitativas."+"\n"+"3. Salir"+"\n")
+    while True:
+        decision = int(input("Ingrese su decision: "))
+        if decision == 1:
+           cantidad_notas = int(input("Ingrese la cantidad de notas que va a promediar: "))
+           print("\n"+f"Su promedio cuantitativo es de: {promedio_notas_cuantitativas(cantidad_notas)}"+"\n")
+        elif decision == 2:
+           print("Hola Mundo") #Aqui toca programar la segunda opcion que es la cualitativa, pero mientras hacemos eso puse este hola mundo para que el codigo no diera error :)
+        elif decision == 3:
+           print("\n"+"¡Hasta luego!"+"\n")
+           break
+        else:
+           print("\n"+"La decision debe ser: 1, 2 o 3."+"\n")
+except:
+  print("")
+  print("*"*15+"ERROR EN EL SISTEMA"+"*"*15)
+  print("")
 
